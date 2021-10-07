73fb86cb2dc9b7395c179fc2dd9ec10e86ebd91e
Dasun Sennath <dasunsennath123@gmail.com>
2021-10-07 12:30:56 +0530

    add user profile API

---
+++ configure.js
@@ -0,0 +1,6 @@
+const config = {
+    'DB_URL' : 'mongodb+srv://dasun:yL1vN642EAPQVDSO@cluster0.twy0a.mongodb.net/Profile?retryWrites=true&w=majority',
+    'secret' : "456-890-87-1902-0674-8893"
+};
+
+module.exports = config;
