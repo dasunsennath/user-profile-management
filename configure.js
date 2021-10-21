10574e5b611e6d06c8889f44f0890d5137c11cb8
Dasun Sennath <69710141+dasunsennath@users.noreply.github.com>
2021-10-21 13:17:44 +0530

    Update configure.js

--- configure.js
+++ configure.js
@@ -1,5 +1,5 @@
 const config = {
-    'DB_URL' : process.env.MONGODB_URL,
+    'DB_URL' : process.env.MONGODB_URL||"mongodb+srv://API:P0wrQbrDYnk3j1xS@cluster0.twy0a.mongodb.net/Profile?retryWrites=true&w=majority",
     'secret' : "456-890-87-1902-0674-8893"
 };
 
